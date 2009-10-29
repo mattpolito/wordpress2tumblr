f4fc7bbdb188816600abebdb5cab3126bd9b4f48
Matt Polito <matt.polito@gmail.com>
2009-10-29 09:58:42 -0500

    changed create post method to account for various arguments

--- tumblr.rb
+++ tumblr.rb
@@ -8,31 +8,38 @@ class Tumblr
   READ_URI  = BASE_URI + '/api/read'
   WRITE_URI = BASE_URI + '/api/write'
   
-  USERNAME  = CONFIG['username']
   EMAIL     = CONFIG['email']
   PASSWORD  = CONFIG['password']
 
   class << self
     def posts(format = nil)
       uri = format.nil? ? READ_URI : READ_URI + "/#{format}"
-      HTTParty.post(uri, :body => { :username => USERNAME, :password => PASSWORD })
+      HTTParty.post(uri, :body => { :email => EMAIL, :password => PASSWORD })
     end
     
-    def create_post(type, title, body, date = nil)
+    def create_post(args) #create_post(type, title, body, date = nil)
       HTTParty.post(WRITE_URI, :body => { 
-        :email    => EMAIL,
-        :password => PASSWORD,
-        :type     => type,
-        :title    => title,
-        :body     => body,
-        :date     => date
+        :email     => EMAIL,
+        :password  => PASSWORD,
+        :type      => args[:type],
+        :title     => args[:title],
+        :body      => args[:body],
+        :tags      => args[:tags],
+        :date      => args[:date],
+        :group     => args[:group],
+        :private   => args[:private]
+        :generator => 'Wordpress 2 Tumblr by Matt Polito'
         })
     end
     
     def import_from_wordpress(file_path)      
       xml = Crack::XML.parse(open(file_path))
       xml['rss']['channel']['item'].each do |item|
-        create_post('regular', item['title'], item['content:encoded'], item['wp:post_date_gmt'])
+        create_post(
+          :type  => 'regular', 
+          :title => item['title'], 
+          :body  => item['content:encoded'], 
+          :date  => item['wp:post_date_gmt'])
         p "Imported: #{item['title']}"
       end
     end
